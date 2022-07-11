101e7a86c9f9e6192de83997701c3f7221cf28db
harteaga <harteaga@SVRWEB>
2022-07-11 13:58:39 -0500

    Se listo las solicitudes aprobadas y en inspección, realizandose el flujo de actualización

--- src/app/credit/application/application-list/application-list.component.ts
+++ src/app/credit/application/application-list/application-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { CaymanAPIService } from 'src/app/services/cayman-api.service';
 import { environment } from 'src/environments/environment';
@@ -12,13 +14,35 @@ export class ApplicationListComponent implements OnInit {
 
   solicitudList$!: Observable<any[]>;
   readonly inspectionAPIUrl = environment.baseUrl;
-  
-  
 
-  constructor(private service: CaymanAPIService) { }
+
+
+  constructor(private service: CaymanAPIService, private cookieService: CookieService) { }
 
   ngOnInit(): void {
-    this.solicitudList$ = this.service.getAll(this.inspectionAPIUrl + '/Application'); 
+    this.solicitudList$ = this.service.getAll(this.inspectionAPIUrl + '/Application');
+
+  }
+
+  inspection(id: any): void {
+    const data = {
+      id: id,
+      userId: this.cookieService.get('userId'),
+      state: 1
+    }
+
+    var opcion = confirm("Está solicitud, pasara a inspeccionarse, ¿de acuerdo?");
+    if (opcion == true) {
+      this.service.put(this.inspectionAPIUrl + '/Application/' + id, data)
+      .subscribe(
+        response => {
+          this.solicitudList$ = this.service.getAll(this.inspectionAPIUrl + '/Application');     
+        },
+        error => {
+          console.log(error);
+        });
+    } 
+
     
   }
 
